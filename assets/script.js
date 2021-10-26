6ce5653673f4a5b083fae4ac1ab84514b9299027
Greg Christiansen <gregchristiansen14@gmail.com>
2021-10-26 17:08:09 -0500

    added variables for the arrays and Declarations

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,27 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Password Generator will provide a password with 8-50 characters based on the criteria 
+
+// Assignment code plus Event Listener to prompt questions when the button is pushed
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+
+// Different combinations of Arrays
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "+", "?", "/", "."];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+// Var Declarations
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+
+
